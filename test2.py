4a5d8ea4ac1ccaabda5adeef6c1c4540a3df5208
Flubarlo <florianeuville@gmail.com>
2020-05-15 23:48:03 +0100

    Ajout d'un menu de selction
    Permet de pouvoir naviguer entre les niveaux.
    Rappel des commandes :
    selection des niveau :
        1(&) -> niveau 1
        2(é) -> niveau 2
        q -> quitter
    pendant les niveau :
        p -> ouvrir porte
        c -> continuer si la porte est ouverte
        r -> retourner au menu sélection
        q -> quitter

--- test2.py
+++ test2.py
@@ -10,7 +10,7 @@ pygame.display.set_caption("Hack this Mifare")
 ############ Boucle des niveaux  #####################
 
 
-def bouclePrincipale():
+def bouclePrincipale(bool1, bool2):
 
 
 
@@ -33,8 +33,8 @@ def bouclePrincipale():
         pygame.key.set_repeat(10, 30)
 
 ########## SELECTION DES NIVEAUX #######################
-        niveau1 = True
-        niveau2 = False
+        niveau1 = bool1
+        niveau2 = bool2
 
         while niveau1 == True:
             fenetre.blit(fenetre_niveau1.fond,(0,0))
@@ -52,13 +52,17 @@ def bouclePrincipale():
                 if event.type == QUIT:
                     pygame.quit()
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_p:
+                    if event.key == pygame.K_p: #ouvre la porte (code a remplacé popur la detection de carte)
                         print("bravo la porte est ouverte")
                         porte.verouille = 0
-                    if event.key == pygame.K_c and porte.verouille == 0:
+                    if event.key == pygame.K_c and porte.verouille == 0:  #Permet de continuer seulement si la porte a été ouverte
                         niveau1 = False
                         niveau2 = True
-                    if event.key == pygame.K_a: #qwerty de base il faut appuyer sur q pour un azerty
+                    if event.key == pygame.K_r :  #Retourne à la selection de niveau
+                        selecteurNiveau()
+                        niveau1 = False
+                        niveau2 = False
+                    if event.key == pygame.K_a: #qwerty de base il faut appuyer sur q pour un azerty Permet de quitter
                         niveau1 = False
                         pygame.quit()
 
@@ -78,36 +82,45 @@ def bouclePrincipale():
             for event in pygame.event.get():
                 if event.type == QUIT:
                     pygame.quit()
-                if event.type == pygame.KEYDOWN:    #code a remplacé popur la detection de carte
+                if event.type == pygame.KEYDOWN:    #ouvre la porte (code a remplacé popur la detection de carte)
                     if event.key == pygame.K_p :    
                         print("bravo la porte est ouverte")
                         porte2.verouille = 0
+                    if event.key == pygame.K_c and porte2.verouille == 0 : #Permet de continuer seulement si la porte a été ouverte
+                        selecteurNiveau() #Comme c'est le dernier niveau on retombe sur la sélection de menu """""""A CHANGER SI CE N'EST PLUS LE DERNIER NIVEAU"""""""""
+                        niveau1 = False
+                        niveau2 = False
+                    if event.key == pygame.K_r :  #Retourne à la selection de niveau
+                        selecteurNiveau()
+                        niveau1 = False
+                        niveau2 = False
                     if event.key == pygame.K_a: #qwerty de base il faut appuyer sur q pour un azerty
                         niveau2 = False
                         pygame.quit()
 
 
 
-""" def selecteurNiveau():
+def selecteurNiveau(): #Premiere interface qui permet de selectionner les niveaux
     gameStart = True
+
     while gameStart == True:
-        fenetre_selection = Terrain("Gameover.png")
-        fenetre = pygame.display.set_mode((800, 530), pygame.RESIZABLE)
-        pygame.key.set_repeat(10, 30)
+        fenetre = pygame.display.set_mode((1000, 661), pygame.RESIZABLE)
+        fenetre_selection = Terrain("selection.png")
         fenetre.blit(fenetre_selection.fond,(0,0))
+        pygame.display.update()
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:    
-                    if event.key == pygame.K_1 :
+                    if event.key == pygame.K_1 : #On appuie sur 1(&) pour choisir le niveau 1
                         bouclePrincipale(True,False)
                         gameStart = False
-                    if event.key == pygame.K_2:
+                    if event.key == pygame.K_2: #On appuie sur 2(é) pour choisir le niveau 2
                         bouclePrincipale(False,True)
                         gameStart = False
-                    if event.key == pygame.K_a:
+                    if event.key == pygame.K_a: #q en azerty pour quitter
                         gameStart = False
-                        pygame.quit() """
+                        pygame.quit() 
 
 
-bouclePrincipale()
+selecteurNiveau()
                 
             
